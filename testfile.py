3ca2ec89fb4142027be61f607ef47358160bee65
djanloo <zzibecu@gmail.com>
2021-09-03 19:31:55 +0200

    Test for 0-approaching pdf
    It turned out the stupid part of the algorithm is me:
    i gave it log_f = 1/x and was surprised by the fact that occurrences 
    didn't go to zero for big x

--- testfile.py
+++ testfile.py
@@ -7,20 +7,21 @@ from matplotlib import pyplot as plt
 from numpy.random import uniform as U
 
 
-bounds = np.ones(2)*5
-bounds = (-bounds , bounds)
+bounds = np.ones(2)
+bounds = (bounds , 100*bounds)
 
 def log_prior(x):
-    x1, x2 = model.unpack_variables(x)
-    return np.log(np.sin(x1) + 2)
+    x0, x1 = model.unpack_variables(x)
+    return np.log(1/x0)
 
 def log_likelihood(x):
     return np.log(np.abs(np.sum(x, axis = -1)))
 
-
+#generate some fucking samples and watch how stupidly fast it is
 my_model = model.Model(log_prior, log_likelihood, bounds)
-sampler = samplers.AIESampler(my_model, 500, nwalkers = 10000)
+sampler = samplers.AIESampler(my_model, 100, nwalkers = 5000)
 
-x = sampler.sample_prior().join_chains(burn_in = 0.1)
-plt.hist(x,bins = 100, histtype = 'step', density = True)
+x = sampler.sample_prior()
+plt.hist(x.join_chains(burn_in = 0.1),bins = 100, histtype = 'step', density = True)
+
 plt.show()
