50ff7f2592503280c9089678a7995ee1c83db042
djanloo <zzibecu@gmail.com>
2021-09-08 16:23:16 +0200

    Found a way to profile cython class methods
    See instructions in profiler_setup.py

---
+++ profiler.py
@@ -0,0 +1,25 @@
+import model
+import numpy as np
+import cy_samplers
+
+
+class MyModel(model.Model):
+    def __init__(self):
+        self.bounds = (-np.ones(2)*10 ,np.ones(2)*10 )
+        super().__init__()
+
+    @model.Model.auto_bound
+    def log_prior(self,x):
+        return 0
+
+    def log_likelihood(self,x):
+        return -0.5*np.sum(x**2,axis = -1)
+
+
+my_model = MyModel()
+
+#initialise evolver sampler
+evolve_sampler = cy_samplers.AIESampler(my_model, 20 ,nwalkers=10)
+
+def lol():
+    evolve_sampler.AIEStep(my_model.log_prior)
