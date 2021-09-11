26c41e4da9ce594af66d653e69af17a33621d669
djanloo <zzibecu@gmail.com>
2021-09-11 17:01:20 +0200

    It's all fucked up but i commit anyway

--- NestedSampling.py
+++ NestedSampling.py
@@ -5,13 +5,11 @@ import test
 from utils  import *
 from matplotlib import pyplot as plt
 from numpy.random import uniform as U
+from numpy.lib import recfunctions as rfn
 from bisect import bisect_left
 from tqdm import trange
 
-import pyximport; pyximport.install()
 import samplers
-
-from functools import lru_cache
 
 np.seterr(divide = 'ignore')
 
@@ -22,45 +20,27 @@ class MyModel(model.Model):
 
     @model.Model.auto_bound
     def log_prior(self,x):
-        return 0
+        return 0#np.log(1/x[...,0])
 
+    @model.Model.destructure
     def log_likelihood(self,x):
         return -0.5*np.sum(x**2,axis = -1)
 
 
 my_model = MyModel()
+
 nlive = 100
-npoints = 100
-points = np.array(0)
-def main():
-    #initialisation of the first nlive points and sorting
-    points = samplers.AIESampler(my_model, 100, nwalkers=nlive ).sample_prior().chain[99]
-    logLs  = my_model.log_likelihood(points)
+npoints = 1000
+evosteps = 70
 
-    points  = points[np.argsort(logLs)]
-    logLs   = np.sort(logLs)
+evo     = samplers.AIESampler(my_model, evosteps , nwalkers = nlive).sample_prior().tail_to_head()
+points  = np.sort(evo.chain[0], order='logL')
 
-    #initialise evolver sampler
-    evolve_sampler = samplers.AIESampler(my_model, 70 ,nwalkers=nlive-1)
+for i in trange(npoints):
+    new_point     = evo.get_new(points['logL'][i])
+    insert_index  = bisect_left(points['logL'],new_point['logL'])
+    points        = np.insert(points, insert_index, new_point)
+    evo.chain[0]  = points[i+1 :]
 
-    for n_generated in trange(npoints):
-
-        evolve_sampler.chain[evolve_sampler.elapsed_time_index] = points[n_generated+1:].copy()
-        new_point = evolve_sampler.sample_prior(Lthreshold = logLs[n_generated])
-
-        #inserts the point in the right place of the ordered list
-        replace_index = bisect_left(logLs, my_model.log_likelihood(new_point))
-        logLs         = np.insert(logLs, replace_index , my_model.log_likelihood(new_point))
-        points        = np.insert(points,  replace_index,  new_point, axis = 0)
-
-        #reset the sampler
-        evolve_sampler.reset()
-
-if __name__ == '__main__':
-    try:
-        globals()[sys.argv[1]]()
-        if sys.argv[2] == '-p':
-            plt.plot(points[:,0], points[:,1])
-            plt.show()
-    except IndexError:
-        pass
+    evo.elapsed_time_index = 0
+logX = -np.linspace(0,1,nlive+npoints)/(nlive+npoints)
