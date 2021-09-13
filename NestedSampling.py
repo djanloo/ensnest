7bdda107734bdca8efe8b23a612151e727147255
djanloo <zzibecu@gmail.com>
2021-09-13 11:54:26 +0200

    IT WORKS
    slightly modified version of NS bewilderingly boosts up performance
    
    The combination of AIESampler with multiple generation of particles 
    (~dinamic NS) is the key

--- NestedSampling.py
+++ NestedSampling.py
@@ -30,37 +30,48 @@ class MyModel(model.Model):
 def main():
     my_model = MyModel()
 
-    nlive = 1000
-    npoints = 1000000
+    nlive = 10000
+    npoints = 100000
+    ng = [0]
     generated = 0
     evosteps = 70
 
     evo     = samplers.AIESampler(my_model, evosteps , nwalkers = nlive).sample_prior().tail_to_head()
     points  = np.sort(evo.chain[0], order='logL')
-    plt.ion()
-    plt.show()
+
     with tqdm(total = npoints) as pbar:
         while generated < npoints:
             _, all          = evo.get_new(points['logL'][generated])
-            plt.figure(1)
-            plt.scatter(all['position'][:,0], all['position'][:,1])
-            plt.title(f"sampling on logL > {points['logL'][generated]} ({generated})")
             insert_index    = np.searchsorted(points['logL'],all['logL'])
             points          = np.insert(points, insert_index, all)
             points          = np.sort(points, order = 'logL')
-
-            plt.figure(2)
-            plt.plot(points['logL'])
-
             evo.chain[0]    = points[-nlive:]
-            breakpoint()
+            ng.append(len(all))
             generated += len(all)
             evo.elapsed_time_index = 0
             pbar.update(len(all))
 
-    logX = -np.linspace(0,1,nlive+generated)/(nlive+generated)
-    plt.plot(logX, points['logL'])
+    ng.append(nlive)
+    ng = np.array(ng)
+
+    #generate the logX values
+    DeltaN = []
+    for i in tqdm(range(len(ng)), desc = 'calculating numbers'):
+        c = ng[i]
+        while c >= 0:
+            DeltaN.append(c)
+            c -= 1
+    DeltaN = np.array(DeltaN)
+    N = DeltaN + nlive
+    logX = np.zeros(len(points))
+    print(f'len DeltaN = {len(DeltaN)} --- len points = {len(points)} ')
+    for i in  tqdm(range(1,len(points)), desc = 'calculating logX'):
+        logX[i] = logX[i-1] - 1/N[i]
+    Z = np.trapz(-np.exp(points['logL']), x = np.exp(logX))
+    print(f'logZ = {Z} , integral = {400*Z}')
+    plt.plot(logX, points['logL'], ls ='' ,marker = '.')
     plt.show()
+
 
 if __name__ == '__main__':
     main()
