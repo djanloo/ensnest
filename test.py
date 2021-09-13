20e808972de407d183ba330d2064c6c9d2da9557
djanloo <zzibecu@gmail.com>
2021-09-13 17:46:40 +0200

    Removed unnecessary files

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-import numpy as np
-import model
-import utils
-
-def rosenbrock(x):
-    x1,x2 = model.to_variables(x)
+def rosenbrock(x1,x2):
     return  - 5* (x2 - x1**2)**2 - 1./20*(1-x1)**2
-
-def uniform(x):
-    return np.zeros(utils.pointshape(x))
