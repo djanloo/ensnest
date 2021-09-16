31c9291692e412204d1be87a26849397a1f3ecd7
djanloo <zzibecu@gmail.com>
2021-09-16 22:22:54 +0200

    Modified workflow for NS
    Added update at each iteration
    Added closing and opening operations
    
    The extrema of integration are not correct tho, missing stripes

--- NestedSampling.py
+++ NestedSampling.py
@@ -16,115 +16,133 @@ np.seterr(divide = 'ignore')
 
 class NestedSampler:
 
-    def __init__(self,model, nlive = 1000, npoints = None, evosteps = 70):
+    def __init__(self,model, nlive = 1000, npoints = np.inf, evosteps = 70):
 
         self.model      = model
         self.nlive      = nlive
         self.evosteps   = evosteps
         self.logZ       = -np.inf
-        self.logX       = None
-        self.logL       = None
+        self.logX       = np.array([0., -1./nlive],        dtype=np.float64)
+        self.logL       = np.array([-np.inf],   dtype=np.float64)
+        self.dlogZ      = None
 
         #since variable nlive is faster:
-        self.N          = None # ~N(t)
-        self.ngen       = None  # number of live points pumped in at each iteration
-        self.generated  = 0     # cumulant for ngen
+        self.N          = np.array([self.nlive],dtype=np.int) # ~N(t)
+        self.ngen       = np.array([0],         dtype=np.int) # number of live points pumped in at each iteration could be non-constant
+        self.generated  = 1    # cumulant for ngen
 
         #initialises the sampler (AIES is the only option currently)
-        self.evo    = samplers.AIESampler(self.model, evosteps , nwalkers = nlive).sample_prior().tail_to_head()
+        self.evo        = samplers.AIESampler(self.model, evosteps , nwalkers = nlive).sample_prior().tail_to_head()
         self.npoints    = npoints
         self.points     = np.sort(self.evo.chain[0], order = 'logL')
 
-        #takes trace of how efficient is the sampling
-        self.mean_duplicates_fraction= 0
+        self.run_time   = np.inf
+        self.run_again  = True
+        self.logZ_error = None
+        self.relative_precision = 1e-2
 
-        self.run_time = np.inf
+        #opening Z increment: takes care of the first point as dZ = (1-X0)*L0
+        self.logZ += (1 - np.exp(-1/nlive))*np.exp(self.points['logL'][0])
 
     def run(self):
         start = time()
-
-        #takes trace of the generated points in case duplicates are excluded
-        self.ngen = [0]
-
+        plt.ion()
+        plt.show()
         with tqdm(total = self.npoints, desc='nested sampling', unit_scale=True , colour = 'blue') as pbar:
 
-            while self.generated + self.nlive < self.npoints:
+            #main loop
+            while self.run_again:
 
-                _, all          = self.evo.get_new(self.points['logL'][self.generated])
-                insert_index    = np.searchsorted(self.points['logL'],all['logL'])
-                self.points     = np.insert(self.points, insert_index, all)
-                self.points          = np.sort(self.points, order = 'logL') #because searchsorted fails sometimes
-                self.evo.chain[0]    = self.points[-self.nlive:]
-                self.ngen.append(len(all))
-                self.generated += len(all)
-                self.mean_duplicates_fraction   += self.evo.duplicate_ratio
-                self.evo.elapsed_time_index     = 0
-                pbar.update(len(all))
+                new             = self.evo.get_new(self.points['logL'][self.generated])
+                insert_index    = np.searchsorted(self.points['logL'],new['logL'])
+                self.points     = np.insert(self.points, insert_index, new)
+                self.points     = np.sort(self.points, order = 'logL') #because searchsorted fails sometimes
 
-        self.ngen = np.array(self.ngen)
+                self.evo.reset(start = self.points[-self.nlive:])      #restarts the sampler giving last live points as initial ensemble
+                self.ngen       = np.append(self.ngen, [len(new)] )
+                self.generated += len(new)
 
-        #generate the logX values
-        jumps  = np.zeros(len(self.points), dtype=np.int)
-        self.N = np.zeros(len(self.points), dtype=np.int)
-        current_index = 0
-        for ng_i in self.ngen:
-            jumps[current_index] = ng_i
-            current_index += ng_i
-
-        self.N[0] = self.nlive
-        for i in range(1,len(self.N)):
-            self.N[i] = self.N[i-1] - 1 + jumps[i-1]
-
-        # X = [1,X0,..., X(n-1), 0]  -> n points + 2 'artificial'
-        # with X0 = worst among N[0] in (0,1)   ~ exp(-1/N[0])
-        #      X1 = worst among N[1] in (0,X0)  ~ exp(-1/N[0] - 1/N[1])
-        #      ecc
-        self.logX = np.zeros(len(self.points)+2)
-        self.logL = np.zeros(len(self.points)+2)
-
-        self.logX[1:-1] = -np.cumsum(1/self.N)
-        self.logX[-1] = -np.inf
-
-        # L =[0,L0, ... , L(n-1), L(n-1)] -> fills last block by duplicating the last L
-        self.logL[1:-1] = self.points['logL']
-        self.logL[-1]   = self.logL[-2]
-        self.logL[0]    = -np.inf
+                self.update()
+                pbar.update(len(new))
 
         self.logZ = np.log(np.trapz(-np.exp(self.logL), x = np.exp(self.logX)))
+        print(f'det integral {np.exp(self.logZ)*self.model.volume}')
 
         self.run_time = time() - start
+        breakpoint()
         self.estimate_Zerror()
+        print(f'Run finished: logZ = {self.logZ} +- {self.dlogZ}')
+        print(f'stoch integral {np.exp(self.logZ)*self.model.volume} +- {np.exp(self.logZ)*self.model.volume*self.dlogZ}')
 
-    @np.vectorize
-    def log_worst_t_among(N):
-        '''Helper function to generate a shrink factors'''
-        return np.log(np.min(np.random.uniform(0,1, size = N)))
+    def update(self):
+        '''updates the value of Z given the current state.
+
+        The number of live points is like:
+
+        nlive,(jump) ~2nlive, 2nlive-1, ... ,nlive, (jump) ~2nlive, ecc.
+
+        This function is called between each pair of jumps. Uses the last ngen value
+        and appends N values.
+        '''
+        #checks if it is a normal update or a closure update
+        print(f"log_max_fut_inc - logZ = {self.points['logL'][-1]+ self.logX[-1] - self.logZ}")
+        relative_increment_condition =  (self.points['logL'][-1]+ self.logX[-1] > np.log(self.relative_precision) + self.logZ)
+        n_points_condition           =  (len(self.points) < self.npoints )
+        self.run_again               =  relative_increment_condition and n_points_condition
+
+        #if it is a closure includes also the last live points in the Z computation
+        Nmax        = self.N[-1]     + self.ngen[-1]
+        inf_index   = self.generated - self.ngen[-1]
+        if self.run_again:
+            Nmin      = self.nlive
+            sup_index = self.generated
+        else:
+            Nmin      = 1
+            sup_index = self.generated + self.nlive
+
+        local_N     = np.flip(np.arange( Nmin , Nmax, dtype = np.int))
+        local_logX  = self.logX[-1] - np.cumsum(1./local_N)
+        local_logL  = self.points['logL'][inf_index:sup_index]
+        self.dlogZ  = np.log(np.trapz( - np.exp(local_logL), x = np.exp(local_logX) ))
+        self.logZ   = np.logaddexp(self.logZ, self.dlogZ)
+        self.logX   = np.append(self.logX, local_logX)
+        self.logL   = np.append(self.logL, local_logL)
+        self.N      = np.append(self.N   , local_N)
+
+        plt.fill_between(local_logX, local_logL , -200 + 0*local_logL)
+
+
+    def _log_worst_t_among(self,N):
+        '''Helper function to generate shrink factors'''
+        return np.log(np.max(np.random.uniform(0,1, size = N)))
 
     def estimate_Zerror(self):
-        '''Estimates the error sampling the shrink variables
+        '''Estimates the error sampling t.
+
+        Very slow.
         '''
         # X[i] = worst among(N[i]) ~(det) exp(-1/N[i])
         # for each array of X calc Z
         # do this many times then avg Z over sample
 
-        # actually, since log_worst_t_among is vestorized
-        # creates copies of N then do all together
-        Nexpanded = np.repeat([self.N], 10, axis = 0)
-        logt = self.log_worst_t_among(Nexpanded)
-
-        # ti = t(N[i])
-        #   X0 = t0*1
-        #   X1 = t1*X0 = t1*t0*1
-        #   X2 = t2*t1*t0 ecc.
-
-        #so logX0 = logt0
-        #   logX1 = logt0 + logt1
-        #   logX2 = logt0 + logt1 + logt2   ecc.
+        Ntimes = 10
+        #-------------------------------
+        start = time()
+        Nexpanded = np.tile(self.N, Ntimes)
+        breakpoint()
+        logt = np.array([self._log_worst_t_among(n) for _ , n in tqdm(np.ndenumerate(Nexpanded), total = Ntimes*len(self.N) ,desc = 'estimating error on logZ')])
+        logt = logt.reshape(-1,len(self.N))
+        #----------------------------------
+        # Nexpanded = np.repeat([self.N],Ntimes, axis = 0)
+        #
+        # logt = self.log_worst_t_among_vec("I have no clue why this has to be put here", Nexpanded)
+        print(f'required {time()-start}')
         logX = np.cumsum(logt, axis = -1)
         logX = np.insert(logX,[0,-1],[0, -np.inf], axis = -1)
-        breakpoint()
 
-
+        logZ_samp  = np.log(-np.trapz(np.exp(self.logL), x = np.exp(logX)))
+        self.logZ  = np.mean(logZ_samp)
+        self.logZ_error = np.std (logZ_samp)
 
 
     def check_prior_sampling(self, logL, evosteps, nsamples):
