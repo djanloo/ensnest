b95082bebdb958943e7b13ac3638fa607800ec9a
djanloo <zzibecu@gmail.com>
2022-03-23 23:02:20 +0100

    Autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,7 @@ import sys
 import mock
 #import ensnest
 
-sys.path.insert(0, os.path.abspath('..'))
-print(f"current path is{sys.path}")
+# sys.path.insert(0, os.path.abspath('..'))
 autodoc_mock_imports = ["numpy", 'matplotlib', 'scipy']
 
 # -- Project information -----------------------------------------------------
